94713e31f5a7285a6706bd03953a7527595e2258
jixiaoxing <1144760915@qq.com>
2020-10-07 21:32:11 +0800

    完成登录注册功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,14 +1,5 @@
-//登录
-var baseurl = 'http://ajax.frontend.itheima.net';
-//生产
-// var baseurl = 'http://ajax.frontend.itheima.net';
-//测试
-// var baseurl = 'http://ajax.frontend.itheima.net';
-
-//拦截所有ajax\post\get请求
-//配置参数
-$.ajaxPrefilter(function(options) {
-    alert(options.url)
-    options.url = baseurl + options.url;
-    alert(options.url)
+$(function() {
+    $.ajaxPrefilter(function(options) {
+        options.url = 'http://ajax.frontend.itheima.net' + options.url
+    })
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,57 +1,42 @@
 $(function() {
-    //1.点击事件
-    $("#link_reg").on('click', function() {
-        $('.login-box').hide();
-        $('.reg-box').show();
+    $('#link-reg').on('click', function() {
+        $(".login").hide();
+        $(".reg").show()
     });
-    $("#link_login").on('click', function() {
-        $('.login-box').show();
-        $('.reg-box').hide();
+    $('#link-login').on('click', function() {
+        $(".login").show();
+        $(".reg").hide()
     })
-
-    //2.登录表单验证
     var form = layui.form;
     form.verify({
-        //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
-        pwd: [
+        pass: [
             /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
         ],
-        //确认密码验证
         repwd: function(value) {
-            var pwd = $('.reg-box input[name=password]').val();
+            var pwd = $('.reg [name=password]').val();
             if (pwd !== value) {
-                return "两次输入密码不一致"
+                return '两次输入密码不一致'
             }
         }
-
-    })
-
-    //3.注册功能
-    var layer = layui.layer;
-    $('#form_reg').on('submit', function(e) {
-        //阻止默认提交行为
+    });
+    $('#form-reg').on('submit', function(e) {
         e.preventDefault();
-        //post
         $.ajax({
             method: 'post',
             url: '/api/reguser',
             data: {
-                username: $(".reg-box [name=username]").val(),
-                password: $(".reg-box [name=password]").val()
+                username: $('.reg [name=username]').val(),
+                password: $('.reg [name=password]').val()
             },
             success: function(res) {
                 if (res.status !== 0) {
                     return layer.msg(res.message)
                 };
-
-                layer.msg('注册成功，请登录');
-                $("#link_login").click();
+                $("#link-login").click();
             }
         })
     });
-
-    //4.
-    $("#form_login").submit(function(e) {
+    $('#form-login').on('submit', function(e) {
         e.preventDefault();
         $.ajax({
             method: 'post',
@@ -61,8 +46,7 @@ $(function() {
                 if (res.status !== 0) {
                     return layer.msg(res.message)
                 };
-                layer.msg('登录成功');
-                localStorage.setItem('token', res.token)
+                localStorage.setItem('token', res.token);
                 location.href = '/index.html'
             }
         })
